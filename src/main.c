6ded577236e0b9191489d4a3220912c601f1f8ef
hamletrpg <hamletgil08@gmail.com>
2025-07-13 23:03:33 -0400

    incomplete chapter, got disrtacted :p

--- src/main.c
+++ src/main.c
@@ -1,9 +1,12 @@
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(int argc, const char* argv[])
 {
+  init_vm();
+
   Chunk chunk;
   init_chunk(&chunk);
   int constant = add_constant(&chunk, 1.2);
@@ -13,6 +16,9 @@ int main(int argc, const char* argv[])
   write_chunk(&chunk, OP_RETURN, 123);
 
   disassemble_chunk(&chunk, "test chunk");
+  
+  interpret(&chunk);
+  free_vm();
   free_chunk(&chunk);
 
   return 0;
