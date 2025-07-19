6eea1b179befcb065d6c1e6fb88912c131f0c5b3
hamletrpg <hamletgil08@gmail.com>
2025-07-18 23:16:25 -0400

    scanner

--- includes/vm.h
+++ includes/vm.h
@@ -2,11 +2,16 @@
 #define clox_vm_h 
 
 #include "chunk.h"
+#include "value.h"
+
+#define STACK_MAX 256
 
 typedef struct 
 {
   Chunk* chunk;
   uint8_t* ip;
+  Value stack[STACK_MAX];
+  Value* stack_top;
 } VM;
 
 typedef enum 
@@ -18,6 +23,8 @@ typedef enum
 
 void init_vm();
 void free_vm();
-InterpretResult interpret(Chunk* chunk);
+InterpretResult interpret(const char* source);
+void push(Value value);
+Value pop();
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,25 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
 #include "vm.h"
 
+static void repl()
+{
+  char line[1024];
+  for (;;)
+  {
+    printf("> ");
+
+    if (!fgets(line, sizeof(line), stdin))
+    {
+      printf("\n");
+      break;
+    }
+
+    interpret(line);
+  }
+}
+
+static char* read_file(const char* path)
+{
+  FILE* file = fopen(path, "rb");
+  if (file == NULL)
+  {
+    fprintf(stderr, "Could not open file \"%s\.\n", path);
+    exit(74);
+  }
+
+  // Seeking to the end before reading
+  fseek(file, 0L, SEEK_END);
+  // Checking the size of the file and saving it in a variable 
+  size_t file_size = ftell(file);
+  // going back at the begining.
+  rewind(file);
+  
+  // we allocate some memory (for my personal project I'll be using some sort of arena/bump allocator)
+  // The bump allocator would be the buffer rather than explicitly calling malloc here
+  char* buffer = (char*)malloc(file_size + 1);
+  if (buffer == NULL)
+  {
+    fprintf(stderr, "Not enough memory to read \"%s\".\n", path);
+    exit(74);
+  }
+  size_t bytes_read = fread(buffer, sizeof(char), file_size, file);
+  if (bytes_read < file_size)
+  {
+    sprintf(stderr, "Could not read file \"%s\.\n", path);
+    exit(74);
+  }
+
+  buffer[bytes_read] = '\0';
+
+  fclose(file);
+  return buffer;
+}
+
+static void run_file(const char* path)
+{
+  char* source = read_file(path);
+  InterpretResult result = interpret(source);
+  free(source);
+
+  if (result == INTERPRET_COMPILE_ERROR) exit(65);
+  if (result == INTERPRET_RUNTIME_ERROR) exit(70);
+}
+
 int main(int argc, const char* argv[])
 {
   init_vm();
 
-  Chunk chunk;
-  init_chunk(&chunk);
-  int constant = add_constant(&chunk, 1.2);
-  write_chunk(&chunk, OP_CONSTANT, 123);
-  write_chunk(&chunk, constant, 123);
+  if (argc == 1)
+  {
+    repl();
+  }
+  else if (argc == 2)
+  {
+    run_file(argv[1]);
+  }
+  else 
+  {
+    fprintf(stderr, "Usage: clox [path]\n");
+    exit(64);
+  }
 
-  write_chunk(&chunk, OP_RETURN, 123);
-
-  disassemble_chunk(&chunk, "test chunk");
-  
-  interpret(&chunk);
   free_vm();
-  free_chunk(&chunk);
 
   return 0;
 }
