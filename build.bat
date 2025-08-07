1c03617e621c06769cdc62de30b5aad18a1d848c
hamletrpg <hamletgil08@gmail.com>
2025-08-07 19:53:44 -0400

    chapter 6

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
-set files=src\memory.c src\main.c src\debug.c src\chunk.c src\value.c src\vm.c src\compiler.c src\scanner.c 
+set files=src\memory.c src\main.c src\debug.c src\chunk.c src\value.c src\vm.c src\compiler.c src\scanner.c src\object.c
 
-CL /Zi /I C:\dev\c++\study_material\crafting_interpreters\includes %files% /link /OUT:my_interpreter.exe
+CL /Zi /I C:\dev\c++\learning_material\crafting_interpreters_study_notes\includes %files% /link /OUT:my_interpreter.exe
